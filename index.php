292c392de9346a4c20dc8646cddbb35d180915b9
Ollie <ojreardon@gmail.com>
2018-06-04 14:25:29 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,16 @@
+/*
+Plugin Name:  Gravity Forms Disable Entries
+Plugin URI:   https://github.com/ojdon/gravity-forms-disable-entries/new/master
+Description:  Disables the entry storage functionality in Gravity Forms for WordPress.
+Version:      20180604
+Author:       Ollie Reardon 
+Author URI:   https://ojdon.github.io
+License:      MIT
+License URI:  https://opensource.org/licenses/MIT
+*/
+
+// Disable all form entry submissions
+add_action( 'gform_after_submission', 'remove_form_entry' );
+function remove_form_entry( $entry ) {
+    GFAPI::delete_entry( $entry['id'] );
+}
